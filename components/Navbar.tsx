582599c8684125d61ae3db248670ea7e918aea2a
Nuttapat Naothaworn <patnao@hotmail.com>
2024-05-28 00:08:03 +0800

    add Navbar for small devices

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { AiOutlineClose, AiOutlineMenu, AiTwotoneMail } from "react-icons/ai";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedin, } from "react-icons/fa";
 
 export default function Navbar() {
   return (
@@ -25,8 +28,66 @@ export default function Navbar() {
               <li className="ml-10 text-sm uppercase hover:border-b">Contact</li>
             </Link>
           </ul>
+
+          {/* for small devices */}
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
         </div>
       </div>
+
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed right-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+              <div className="flex w-full items-center justify-between">
+                <Image src="../public/assets/next.svg" alt="/" width="87" height="35" />
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                  <AiOutlineClose />
+                </div>
+              </div>
+              <div className="border-b border-gray-300 my-4">
+                <p className="w-[85%] md:w-[90%] py-4">Build something cool</p>
+              </div>
+          </div>
+          <div className="py-4 flex flex-col">
+            <ul className="uppercase">
+              <Link href={"/"}>
+                <li className="py-4 text-sm">Home</li>
+              </Link>
+              <Link href={"/"}>
+                <li className="py-4 text-sm">About</li>
+              </Link>
+              <Link href={"/"}>
+                <li className="py-4 text-sm">Skills</li>
+              </Link>
+              <Link href={"/"}>
+                <li className="py-4 text-sm">Projects</li>
+              </Link>
+              <Link href={"/"}>
+                <li className="py-4 text-sm">Contacts</li>
+              </Link>
+            </ul>
+            <div className="py-40">
+              <p className="uppercase tracking-widest text-teal-500">Get in touch</p>
+              <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaLinkedin/>
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <FaGithub/>
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <AiTwotoneMail/>
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300">
+                  <BsPersonLinesFill/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
